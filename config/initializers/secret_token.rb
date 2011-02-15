44c482fc8986edfd991b893d81e15941317243b7
stickboy <pete.sowerbutts@gmail.com>
2011-02-15 14:54:13 -0800

    initial app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BandSchedulerBdd::Application.config.secret_token = '2989b735f9e58586ba547afad600dccdd946831c6d957afbafdac0f5219d37c8766e2a02445ec1aefd107870741cd5a1f8fe0c302474cf99cf66a6f7a97c1efe'
